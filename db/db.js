672e91e81091cd791b2717ec7257dd438fb31e5a
user <user@users-MacBook-Pro.local>
2021-10-19 11:31:25 +0300

    Add property to DB config

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/notificationsDB', {
-    useNewUrlParser: true
-},
-err => {
-    if (err) {
-        console.log(`DB connection error: ${err}`)
-    } else {
-        console.log('DB connection successful')
-    }
+const MONGO_URI = 'mongodb://localhost:27017/notificationsDB';
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log("error connecting database: ", err));
+
+
+module.exports = mongoose; 
