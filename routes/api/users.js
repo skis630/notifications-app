25c6b0bdc2b6c71efa49d57e38cbb4558f27eba4
user <user@users-MacBook-Pro.local>
2021-10-21 00:07:32 +0300

    Add msg duration & msg interval when new user added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,44 +1,66 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../db/models').User;
+const userModel = require('../../db/models');
+const User = userModel.User;
+const userSchema = userModel.usersSchema;
 const sanitize = require('mongo-sanitize');
-const uuid = require('uuid');
-const callback = require('../../utils').callback;
+const between = require('../../utils').between;
+// const uuid = require('uuid');
+// const callback = require('../../utils').callback;
 
 
-router.put("/:id/clicked_notifications", (req, res) => {
+router.put("/:id/clicked_notifications", async (req, res) => {
     const newClickedNotification = sanitize(req.body);
     const id = sanitize(req.params.id);
 
-    User.findById(id, (err, user) => {
+    await User.findById(id, async (err, user) => {
         console.log(user, id);
         if (!err && user) {
             console.log(user.clicked_notifications);
             user.clicked_notifications.push(newClickedNotification);
-            user.save(error => {
+            await user.save(error => {
                 if (error) {
                     console.log("error: ", error);
+                    res.send("Error updating clicked notifications")
+                } else {
+                    res.json(user)
                 }
             })
         } else {
             console.log("error or no result found: ", err, user);
+            res.send("Error finding user")
         }
     })
 
-    res.end();
 })
 
 router.post("/", async (req, res) => {
-    await User.create({ clicked_notifications: [] }, (err, user) => {
-        if (err) {
-            res.status(500);
-            res.send("Error in creating new user")
-            console.log("Error: ", err)
-        } else {
-            console.log(user);
-            res.json(user)
-        }
-    });
+    const userSchemaObj = userSchema.obj;
+    // Compute notification duraton for user
+    const minDuration = userSchemaObj.notification_duration.min;
+    const maxDuration = userSchemaObj.notification_duration.max;
+    const msgDuration = between(minDuration, maxDuration);
+
+    // Compute notifications interval for user
+    const minInterval = userSchemaObj.notification_interval.min;
+    const maxInterval = userSchemaObj.notification_interval.max;
+    const msgInterval = between(minInterval, maxInterval);
+
+    await User.create(
+        { 
+            clicked_notifications: [],
+            notification_duration: msgDuration,
+            notification_interval: msgInterval 
+        }, (err, user) => {
+            if (err) {
+                res.status(500);
+                res.send("Error in creating new user")
+                console.log("Error: ", err)
+            } else {
+                console.log(user);
+                res.json(user)
+            }
+        });
         
 })
 
