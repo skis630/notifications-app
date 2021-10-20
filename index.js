aa81a2f47a5a356fcbebc728591c9efe30a935be
user <user@users-MacBook-Pro.local>
2021-10-20 19:15:23 +0300

    Add notifications route to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { urlencoded } = require('body-parser');
 const express = require('express');
 const bodyParser = require('body-parser');
 var users = require('./routes/api/users');
+var notifications = require('./routes/api/notifications');
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/users", users);
+app.use("/notifications", notifications)
 //app.use("/", (req, res) => res.end("Dummy server"));
 
 
