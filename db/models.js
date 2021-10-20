5aa40829cce8d6f651f63c35ddfd096f38621852
user <user@users-MacBook-Pro.local>
2021-10-20 19:15:09 +0300

    Re-organize models code for readability

--- db/models.js
+++ db/models.js
@@ -1,16 +1,18 @@
 const mongoose = require('./db');
 
 
+// User model
 const notificationsSchema =  new mongoose.Schema({
     type: String,
     text: String
-}, { collection: "users", typeKey: '$type' });
+}, { collection: "notifications", typeKey: '$type' });
+const Notification = mongoose.model('Notification', notificationsSchema);
+
+// Notifications model
 const usersSchema = new mongoose.Schema({
     clicked_notifications: [{ id: String, type: String, text: String }]
 }, { collection: "users", typeKey: '$type' });
-
 const User = mongoose.model('User', usersSchema);
-const Notification = mongoose.model('Notification', notificationsSchema);
 
 
 module.exports = {
