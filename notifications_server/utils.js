ac580fc3f41069f1258a71a3bb96fceddab2c00a
user <user@users-MacBook-Pro.local>
2021-10-24 16:07:26 +0300

    Server util functions

---
+++ notifications_server/utils.js
@@ -0,0 +1,21 @@
+
+
+// const callback = (err, result) => {
+//     if (!err) {
+//         console.log(result);
+//     } else {
+//         console.log(err);
+//     }
+// }
+
+const between = (min, max) => {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+  }
+
+
+module.exports = {
+    // callback,
+    between
+}
