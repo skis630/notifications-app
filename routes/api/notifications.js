a915d9843726ef35ebf8bbba10a0df9c6f152677
user <user@users-MacBook-Pro.local>
2021-10-24 15:08:10 +0300

    Remove commented line

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Notification = require('../../db/models').Notification;
-const sanitize = require('mongo-sanitize');
 
 
 router.get("/", (req, res) => {
